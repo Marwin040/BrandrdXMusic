e557150099df6cc07f2353037de46a46ec1cd1fa
Marwin040 <markusmarwin00@gmail.com>
2024-11-06 20:13:23 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "ƨσʋяcɛ", url=f"https://github.com/CyberPixelPro/AviaxMusic"
                     )
                 ]
             ]
